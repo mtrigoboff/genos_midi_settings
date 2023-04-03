0e844e922cc34e01ac683a1a36875211c4774376
Michael Trigoboff <michael.trigoboff@pcc.edu>
2023-04-02 18:44:34 -0700

    code refinements

--- print_gms.py
+++ print_gms.py
@@ -1,26 +1,29 @@
 import struct
 
-FILE_HDR_LGTH =			0x024
-MIDI_SETTINGS_LGTH =	0x154
+FILE_HDR_LGTH = 36  # == 0x024
+SETTINGS_LGTH = 340  # == 0x154
+SETTINGS_NAME_LGTH = 41  # == 0x029
+
 
 def main(file_name):
-	# open file
-	try:
-		file_stream = open(file_name, 'rb')
-	except FileNotFoundError:
-		print(f'could not open file: {file_name}')
-		return
+    # open file
+    try:
+        file_stream = open(file_name, 'rb')
+    except FileNotFoundError:
+        return f'could not open file: {file_name}'
 
-	# read file header
-	file_hdr = file_stream.read(FILE_HDR_LGTH)
+    # read file header
+    file_hdr = file_stream.read(FILE_HDR_LGTH)
 
-	# read individual settings
-	for i in range(10):
-		setting_name_bytes, = struct.unpack('> 32s 308x', file_stream.read(MIDI_SETTINGS_LGTH))
-		print(setting_name_bytes.decode('ascii').rstrip('\x00'))
+    # read individual settings
+    for i in range(10):
+        setting_name_bytes, = \
+        	struct.unpack(f'> {SETTINGS_NAME_LGTH}s {SETTINGS_LGTH - SETTINGS_NAME_LGTH}x',
+                          file_stream.read(SETTINGS_LGTH))
+        print(setting_name_bytes.decode('ascii').rstrip('\x00'))
 
-
-	file_stream.close()
+    file_stream.close()
+    return ''
 
 if __name__ == '__main__':
     main('genos_files/MIDISetupPreset.msu')
