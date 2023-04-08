9afe928182f6233ffd1058bb97dbfc65f37147d4
Michael Trigoboff <michael.trigoboff@pcc.edu>
2023-04-07 21:05:05 -0700

    started work on sysex msg settings

--- print_gms.py
+++ print_gms.py
@@ -10,42 +10,81 @@ class MIDISettings:
 	# settings struct layout
 	SETTINGS_LGTH =			340		# == 0x154
 	SETTINGS_NAME_LGTH =	 41		# == 0x029
-	PADDING1 =				112		# == 0x070
+	PADDING1 =				111		# == 0x070
+	LOCAL_CONTROL =			  1
 	CLOCK_LGTH =			  1
 	TRANSMIT_CLOCK_LGTH =	  1
 	TRANSPOSE_MIDI_LGTH =  	  1
 	START_STOP =  			  1
-	PADDING_LAST =			SETTINGS_LGTH - SETTINGS_NAME_LGTH - PADDING1 \
-							- CLOCK_LGTH - TRANSMIT_CLOCK_LGTH - TRANSPOSE_MIDI_LGTH - START_STOP
+	SYS_EX_MSG =			  1
+	CHORD_SYS_EX_MSG =		  1
+	PADDING_LAST =			SETTINGS_LGTH - SETTINGS_NAME_LGTH - PADDING1 - LOCAL_CONTROL \
+							- CLOCK_LGTH - TRANSMIT_CLOCK_LGTH - TRANSPOSE_MIDI_LGTH - START_STOP \
+							- SYS_EX_MSG - CHORD_SYS_EX_MSG
 
-	separator = '-----------------------------------\n'
+	separator = '-------------------------------------------------------------------------\n'
+
+	_setting_labels = ('Clock', 'Transmit Clock', 'Transpose MIDI Input', 'Start/Stop', \
+		    		   'Local Control', 'System Exclusive Message', 'Chord System Exclusive Message')
+	_setting_labels = [f'{sl + ":":32s}' for sl in _setting_labels]
+	_setting_labels = [f'{"":32s}'] + _setting_labels
 
 	_clock_labels = ('Internal', 'MIDI A', 'MIDI B', 'USB1', 'USB2', 'Wireless LAN')
 	_off_on_labels = ('Off', 'On')
 	_start_stop_labels = ('Song', 'Style')
-
+	_local_ctl_labels = ((('Left', 0x10), ('Right1', 0x08), ('Right2', 0x04), ('Right3', 0x02)), \
+		    			 (('Style', 0x40), ('Song', 0x80), ('M.Pad', 0x20)))
 	def __init__(self, settings_bytes):
 
-		settings_name_bytes, self._clock, self._xmit_clock, self._xpose_midi, self._start_stop = \
+		settings_name_bytes, self._local_control, self._clock, self._xmit_clock, \
+		self._xpose_midi, self._start_stop, self._sys_ex_msg, self._chord_sys_ex_msg = \
 			struct.unpack(f'> \
 		 					{self.SETTINGS_NAME_LGTH}s \
 							{self.PADDING1}x \
+		 					{self.LOCAL_CONTROL}B \
 		 					{self.CLOCK_LGTH}B \
 		 					{self.TRANSMIT_CLOCK_LGTH}B \
 		 					{self.TRANSPOSE_MIDI_LGTH}B \
 		 					{self.START_STOP}B \
+		 					{self.SYS_EX_MSG}B \
+		 					{self.CHORD_SYS_EX_MSG}B \
 		 					{self.PADDING_LAST}x',
 						  settings_bytes)
 
 		self.name = settings_name_bytes.decode('ascii').rstrip('\x00')
 
+	def local_ctl(self, lc_flags, lc_labels):
+		local_ctl_str = ''
+		first = True
+		for lbl in lc_labels:
+			if first:
+				first = False
+			else:
+				local_ctl_str += ' '
+			local_ctl_str += lbl[0] + ':'
+			if lbl[1] & lc_flags == 0:
+				local_ctl_str += self._off_on_labels[0]
+			else:
+				local_ctl_str += self._off_on_labels[1]
+		local_ctl_str += '\n'
+		return local_ctl_str
+
 	def __str__(self):
-		return f'{"Genos MIDI Setting:":26s} {self.name}\n' + 								\
-			   self.separator +																\
-			   f'{"Clock:":26s} {self._clock_labels[self._clock]}\n' +						\
-			   f'{"Transmit Clock:":26s} {self._off_on_labels[self._xmit_clock]}\n'			\
-			   f'{"Transpose MIDI Input:":26s} {self._off_on_labels[self._xpose_midi]}\n'	\
-			   f'{"Start/Stop:":26s} {self._start_stop_labels[self._start_stop]}\n'
+
+		local_ctl_str = ''
+		local_ctl_str += self.local_ctl(self._local_control, self._local_ctl_labels[0])
+		local_ctl_str += self._setting_labels[0] + ' '
+		local_ctl_str += self.local_ctl(self._local_control, self._local_ctl_labels[1])
+
+		return f'{"Genos MIDI Setting:":32s} {self.name}\n' + 									\
+			   self.separator +																	\
+			   f'{self._setting_labels[1]} {self._clock_labels[self._clock]}\n' +				\
+			   f'{self._setting_labels[2]} {self._off_on_labels[self._xmit_clock]}\n' +			\
+			   f'{self._setting_labels[3]} {self._off_on_labels[self._xpose_midi]}\n' +			\
+			   f'{self._setting_labels[4]} {self._start_stop_labels[self._start_stop]}\n' +		\
+			   f'{self._setting_labels[5]} {local_ctl_str}\n' +									\
+			   f'{self._setting_labels[6]} {hex(self._sys_ex_msg)}\n' +		\
+			   f'{self._setting_labels[7]} {hex(self._chord_sys_ex_msg)}\n'
 
 def main(genos_file, analyze):
 
