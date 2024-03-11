6b111eaffa080a02dcb50923a06cccb288bee612
Michael Trigoboff <mtrigoboff@gmail.com>
2024-03-10 18:01:06 -0700

    prints Genos2 file, not quite correctly

--- print_gms.py
+++ print_gms.py
@@ -1,30 +1,31 @@
 import os, os.path, struct, sys
 
-# file header layout
-HDR_LGTH =			 	 36		# == 0x024
-HDR_STR_LGTH =			 32		# == 0x020
-HDR_FLAGS_LGTH =		  1		# one 2-byte short
+# file layout
+FILE_LGTH =			 	 	298		# == 0x024
+HDR_LGTH =			 	 	35		# == 0x024
 
 class MIDISettings:
 
-	# settings struct layout
-	SETTINGS_LGTH =			340		# == 0x154
-	SETTINGS_NAME_LGTH =	 41		# == 0x029
-	PADDING1 =				111		# == 0x070
-	LOCAL_CONTROL =			  1
-	CLOCK_LGTH =			  1
-	TRANSMIT_CLOCK_LGTH =	  1
-	TRANSPOSE_MIDI_LGTH =  	  1
-	START_STOP =  			  1
-	SYSEX_MSGS =			  1		# 2-byte short
-	TRANSMIT = 				 34		# 2-byte shorts
-	RECEIVE = 				 32		# 2-byte shorts
-	PADDING2 =				  1
-	ON_BASS_NOTE =			  1		# one 4-byte int
-	CHORD_DETECT =			  1		# one 4-byte int
-	PADDING_LAST =			SETTINGS_LGTH - SETTINGS_NAME_LGTH - PADDING1 - LOCAL_CONTROL					\
-							- CLOCK_LGTH - TRANSMIT_CLOCK_LGTH - TRANSPOSE_MIDI_LGTH - START_STOP			\
-							- 2 * SYSEX_MSGS - 2 * TRANSMIT - 2 * RECEIVE - PADDING2						\
+	# settings struct layout (bytes, except where noted)
+	SETTINGS_LGTH =			FILE_LGTH - HDR_LGTH
+	LOCAL_CONTROL =			 1		# [0]
+	CLOCK =					 1		# [1]
+	TRANSMIT_CLOCK =		 1		# [2]
+	TRANSPOSE_MIDI = 	 	 1		# [3]
+	START_STOP =  			 1		# [4]
+	SYSEX_XMIT =  			 1		# [5]
+	SYSEX_CHORD_XMIT =  	 1		# [6]
+	SYSEX_RCV =  			 1		# [7]
+	SYSEX_CHORD_RCV =  		 1		# [8]
+	PADDING1 =				 4		# [9 - 12]
+	TRANSMIT = 				34		# [13] (2-byte shorts)
+	RECEIVE = 				32		# [70] (2-byte shorts)
+	PADDING2 =				 1
+	ON_BASS_NOTE =			 1		# one 4-byte int
+	CHORD_DETECT =			 1		# one 4-byte int
+	PADDING_LAST =			SETTINGS_LGTH - LOCAL_CONTROL - CLOCK - TRANSMIT_CLOCK - TRANSPOSE_MIDI			\
+							- START_STOP - SYSEX_XMIT - SYSEX_CHORD_XMIT - SYSEX_RCV						\
+							- SYSEX_CHORD_RCV - PADDING1 - 2 * TRANSMIT - 2 * RECEIVE - PADDING2			\
 							- 4 * ON_BASS_NOTE - 4 * CHORD_DETECT
 
 	seperator = '----------------------------------------------------------------------------\n'
@@ -36,6 +37,7 @@ class MIDISettings:
 							 'On Bass Note', 'Chord Detect')
 	_setting_labels =		[f'{sl + ":":33s}' for sl in _setting_labels]		# pad labels to same width
 	_setting_labels[0] =	' ' + _setting_labels[0][1:]						# replace colon with space
+	# _setting_labels[0] is a blank label to maintain identing for multiple-line setting printout
 
 	_clock_labels =			('Internal', 'MIDI A', 'MIDI B', 'USB1', 'USB2', 'Wireless LAN')
 	_off_on_labels =		('Off', 'On')
@@ -69,31 +71,21 @@ class MIDISettings:
 
 		self._file_name = file_name
 
-		settings_name_bytes, self._local_control, self._clock, self._xmit_clock,							\
-		self._xpose_midi, self._start_stop, self._sysex_msgs, self._transmit, self._receive,				\
-		self._on_bass_note, self._chord_detect =															\
-			struct.unpack(f'> 																				\
-		 					{self.SETTINGS_NAME_LGTH}s														\
-							{self.PADDING1}x																\
-		 					{self.LOCAL_CONTROL}B															\
-		 					{self.CLOCK_LGTH}B																\
-		 					{self.TRANSMIT_CLOCK_LGTH}B														\
-		 					{self.TRANSPOSE_MIDI_LGTH}B														\
-		 					{self.START_STOP}B																\
-		 					{self.SYSEX_MSGS}H																\
-							{2 * self.TRANSMIT}s															\
-							{2 * self.RECEIVE}s																\
-							{self.PADDING2}x																\
-							{self.ON_BASS_NOTE}I															\
-							{self.CHORD_DETECT}I															\
-		 					{self.PADDING_LAST}x',
-						  settings_bytes)
-
-		self.name = settings_name_bytes.decode('ascii').rstrip('\x00')
-
+		self._local_control, self._clock, self._xmit_clock,	self._xpose_midi, self._start_stop,				\
+		self._sysex_xmit, self._sysex_chord_xmit, self._sysex_rcv, self._sysex_chord_rcv,					\
+		self._transmit, self._receive, self._on_bass_note, self._chord_detect =								\
+			struct.unpack(
+				f'> {self.LOCAL_CONTROL}B {self.CLOCK}B {self.TRANSMIT_CLOCK}B'
+				+ f' {self.TRANSPOSE_MIDI}B {self.START_STOP}B'
+				+ f' {self.SYSEX_XMIT}B {self.SYSEX_CHORD_XMIT}B'
+				+ f' {self.SYSEX_RCV}B {self.SYSEX_CHORD_RCV}B {self.PADDING1}x'
+				+ f' {2 * self.TRANSMIT}s {2 * self.RECEIVE}s {self.PADDING2}x'
+				+ f' {self.ON_BASS_NOTE}I {self.CHORD_DETECT}I'
+				+ f' {self.PADDING_LAST}x'
+				, settings_bytes)
+		
 	def local_ctl(self, flags, labels):
 		local_ctl_str = ''
-		first = True
 		local_ctl_setting = ''
 		for lbl in labels:
 			local_ctl_setting = lbl[0] + ':'
@@ -105,19 +97,6 @@ class MIDISettings:
 		local_ctl_str += '\n'
 		return local_ctl_str
 
-	def sysex_msgs(self, flags):
-		byte_masks = (0xFF00, 0x00FF)
-		sysex_msgs_str = ''
-		for i in range(2):
-			sysex_msgs_str += self._xmit_rcv_labels[i] + ':'
-			if flags & byte_masks[i] == 0:
-				sysex_msgs_str += f'{self._off_on_labels[0]:3s}'
-			else:
-				sysex_msgs_str += f'{self._off_on_labels[1]:3s}'
-			if i == 0:
-				sysex_msgs_str += ' '
-		return sysex_msgs_str
-	
 	def transmit(self, flags):
 		part_channel_str = ''
 		c = 0
@@ -173,24 +152,24 @@ class MIDISettings:
 						+ self._setting_labels[0]															\
 						+ self.local_ctl(self._local_control, self._local_ctl_labels[1])
 		
+			# f'{self._setting_labels[2]}{self.name}\n' +
 		return																								\
 			f'{self._setting_labels[1]}{self._file_name}\n' + 												\
-			f'{self._setting_labels[2]}{self.name}\n' + 													\
 			self.seperator +																				\
 			f'{self._setting_labels[3]}{self._clock_labels[self._clock]}\n' +								\
 			f'{self._setting_labels[4]}{self._off_on_labels[self._xmit_clock]}\n' +							\
 			f'{self._setting_labels[5]}{self._off_on_labels[self._xpose_midi]}\n' +							\
 			f'{self._setting_labels[6]}{self._start_stop_labels[self._start_stop]}\n' +						\
 			f'{self._setting_labels[7]}{local_ctl_str}' +													\
-			f'{self._setting_labels[8]}{self.sysex_msgs(self._sysex_msgs & 0x8080)}\n' +					\
-			f'{self._setting_labels[9]}{self.sysex_msgs(self._sysex_msgs & 0x0808)}\n' +					\
+			f'{self._setting_labels[8]}{self._off_on_labels[self._sysex_xmit]}\n' +							\
+			f'{self._setting_labels[9]}{self._off_on_labels[self._xmit_clock]}\n' +							\
 			f'{self._setting_labels[10]}{self.transmit(self._transmit)}' +									\
 			f'{self._setting_labels[11]}{self.receive(self._receive)}' +									\
 			f'{self._setting_labels[12]}{self.on_bass_note_chord_detect(self._on_bass_note)}' +				\
 			f'{self._setting_labels[13]}{self.on_bass_note_chord_detect(self._chord_detect)}' +				\
 			f'\n\n{self._may_be_inaccurate}'
 
-def print_genos_midi_settings(genos_file, folder_suffix, analyze=False):
+def print_genos_midi_settings(genos_file, analyze=False):
 
 	global console_out
 	console_out = sys.stdout
@@ -200,7 +179,7 @@ def print_genos_midi_settings(genos_file, folder_suffix, analyze=False):
 	print(f'Genos File: {genos_file_name}', file=console_out)
 
 	# open Genos file
-	if genos_file_ext != '.msu':
+	if genos_file_ext != '.mis':
 		return f'incorrect file type: "{genos_file_ext}"'
 	try:
 		genos_file_stream = open(genos_file, 'rb')
@@ -208,59 +187,29 @@ def print_genos_midi_settings(genos_file, folder_suffix, analyze=False):
 		return f'could not open file: "{genos_file}"'
 
 	# read file header
-	hdr_str, settings_flags = \
-		struct.unpack(f'> {HDR_STR_LGTH}s {HDR_FLAGS_LGTH}H 2x',
-					  genos_file_stream.read(HDR_LGTH))
-	flags_str = f'{settings_flags:2X}'.zfill(4)				# 4 hex digits via zero fill if needed
-	if analyze:
-		print(f'{hdr_str}, 0x{flags_str}', file=console_out)
+	genos_file_stream.read(HDR_LGTH)
 
-	# set up directory for output files
-	settings_file_dir = os.path.join(genos_file_path, genos_file_name + folder_suffix)
-	if os.path.isdir(settings_file_dir):
-		if not analyze:
-			for root, _, files in os.walk(settings_file_dir):	# delete previous directory contents
-				for f in files:
-					os.unlink(os.path.join(root, f))
-	else:
-		os.mkdir(settings_file_dir)
+	settings_bytes = genos_file_stream.read(MIDISettings.SETTINGS_LGTH)
 
-	# setting_flags = 0x301
+	# unpack settings bytes
+	settings = MIDISettings(genos_file_name, settings_bytes)
 
-	# read individual settings
-	# (settings slots in file are filled in reverse order from end of file)
-	settings_mask = 0x0001
-	for i in range(10):
-		settings_bytes = genos_file_stream.read(MIDISettings.SETTINGS_LGTH)
-		if settings_flags & settings_mask != 0:
-
-			# unpack settings bytes
-			settings = MIDISettings(genos_file_name, settings_bytes)
-
-			if analyze:
-				# write out settings bytes for comparison
-				settings_bytes_file = open(os.path.join(settings_file_dir, settings.name + '.stg'), 'wb')
-				settings_bytes_file.write(settings_bytes)
-				settings_bytes_file.close()
-
-			# print settings info
-			settings_txt_file = open(os.path.join(settings_file_dir, settings.name + '.txt'), 'w')
-			sys.stdout = settings_txt_file
-			print(f'Setting:    {settings.name}', file=console_out)
-			print(settings)
-			settings_txt_file.close()
-			sys.stdout = console_out
-
-		settings_mask <<= 1
+	# print settings info
+	settings_txt_file = open(os.path.join(genos_file_name + '.txt'), 'w')
+	sys.stdout = settings_txt_file
+	print(f'Setting:    {genos_file_name}', file=console_out)
+	print(settings)
+	settings_txt_file.close()
+	sys.stdout = console_out
 
 	genos_file_stream.close()
 
 	return ''
 
 if __name__ == '__main__':
-	if len(sys.argv) > 3:
-		ret_str = print_genos_midi_settings(sys.argv[1], sys.argv[2], True)
+	if len(sys.argv) > 2:
+		ret_str = print_genos_midi_settings(sys.argv[1], True)
 	else:
-		ret_str = print_genos_midi_settings(sys.argv[1], sys.argv[2])
+		ret_str = print_genos_midi_settings(sys.argv[1])
 	if ret_str != '':
 		print(f'print_genos_midi_settings() -> {ret_str}', file=console_out)
