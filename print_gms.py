b7be29eeccb4257b217ffadb3eeaf86e4db836c4
Michael Trigoboff <mtrigoboff@gmail.com>
2024-03-12 23:09:43 -0700

    release candidate 1 for Genos2

--- print_gms.py
+++ print_gms.py
@@ -4,33 +4,35 @@ import os, os.path, struct, sys
 version = [1, 0, 0]
 app_hdr_str = f'Print Genos2 MIDI Settings File  (version {version[0]}.{version[1]}.{version[2]})'
 
-# file layout
+# Genos2 file layout
 FILE_LGTH =			 	 	298
 HDR_LGTH =			 	 	35
 
 class MIDISettings:
 
 	# settings struct layout (bytes, except where noted)
-	SETTINGS_LGTH =			FILE_LGTH - HDR_LGTH
-	LOCAL_CONTROL =			 1		# [0]
-	CLOCK =					 1		# [1]
-	TRANSMIT_CLOCK =		 1		# [2]
-	TRANSPOSE_MIDI = 	 	 1		# [3]
-	START_STOP =  			 1		# [4]
-	SYSEX_XMIT =  			 1		# [5]
-	SYSEX_CHORD_XMIT =  	 1		# [6]
-	SYSEX_RCV =  			 1		# [7]
-	SYSEX_CHORD_RCV =  		 1		# [8]
-	PADDING1 =				 4		# [9 - 12]
-	TRANSMIT = 				34		# [13] (2-byte shorts)
-	RECEIVE = 				32		# [70] (2-byte shorts)
-	PADDING2 =				 1
-	ON_BASS_NOTE =			 1		# one 4-byte int
-	CHORD_DETECT =			 1		# one 4-byte int
+	SETTINGS_LGTH =			FILE_LGTH - HDR_LGTH				# offsets don't include HDR_LGTH
+	LOCAL_CONTROL =			 1		# [0]						offset   0
+	CLOCK =					 1		# [1]						offset   1
+	TRANSMIT_CLOCK =		 1		# [2]						offset   2
+	TRANSPOSE_MIDI = 	 	 1		# [3]						offset   3
+	START_STOP =  			 1		# [4]						offset   4
+	SYSEX_XMIT =  			 1		# [5]						offset   5
+	SYSEX_CHORD_XMIT =  	 1		# [6]						offset   6
+	SYSEX_RCV =  			 1		# [7]						offset   7
+	SYSEX_CHORD_RCV =  		 1		# [8]						offset   8
+	PADDING1 =				 4		# [9 - 12]					offset   9
+	TRANSMIT = 				34		# [13] (2-byte shorts)		offset  13
+	PADDING2 =				 4		#							offset  81
+	RECEIVE = 				32		# [70] (2-byte shorts)		offset  85
+	PADDING3 =				 4
+	ON_BASS_NOTE =			32
+	PADDING4 =				 4
+	CHORD_DETECT =			32
 	PADDING_LAST =			SETTINGS_LGTH - LOCAL_CONTROL - CLOCK - TRANSMIT_CLOCK - TRANSPOSE_MIDI			\
 							- START_STOP - SYSEX_XMIT - SYSEX_CHORD_XMIT - SYSEX_RCV						\
-							- SYSEX_CHORD_RCV - PADDING1 - 2 * TRANSMIT - 2 * RECEIVE - PADDING2			\
-							- 4 * ON_BASS_NOTE - 4 * CHORD_DETECT
+							- SYSEX_CHORD_RCV - PADDING1 - 2 * TRANSMIT - PADDING2 - 2 * RECEIVE			\
+							- PADDING3 - ON_BASS_NOTE - PADDING4 - CHORD_DETECT
 
 	seperator = '----------------------------------------------------------------------------\n'
 
@@ -80,8 +82,8 @@ class MIDISettings:
 				+ f' {self.TRANSPOSE_MIDI}B {self.START_STOP}B'
 				+ f' {self.SYSEX_XMIT}B {self.SYSEX_CHORD_XMIT}B'
 				+ f' {self.SYSEX_RCV}B {self.SYSEX_CHORD_RCV}B {self.PADDING1}x'
-				+ f' {2 * self.TRANSMIT}s {2 * self.RECEIVE}s {self.PADDING2}x'
-				+ f' {self.ON_BASS_NOTE}I {self.CHORD_DETECT}I'
+				+ f' {2 * self.TRANSMIT}s {self.PADDING2}x {2 * self.RECEIVE}s {self.PADDING3}x'
+				+ f' {self.ON_BASS_NOTE}s  {self.PADDING4}x {self.CHORD_DETECT}s'
 				+ f' {self.PADDING_LAST}x'
 				, settings_bytes)
 		
@@ -126,30 +128,24 @@ class MIDISettings:
 		return channel_part_str
 
 	def on_bass_note_chord_detect(self, flags):
-
-		def obn_cd_port(self, flags):
-			mask = 0x0001
-			obn_cd_port_str = ''
-			for i in range(1, 17):
-				if i == 9:
-					obn_cd_port_str += '\n' + self._setting_labels[0] + spacer
-				obn_cd_port_str += f'{i:2d}:'
-				if flags & mask == 0:
-					obn_cd_port_str += f'{self._off_on_labels[0]:3s}'
-				else:
-					obn_cd_port_str += f'{self._off_on_labels[1]:3s}'
-				obn_cd_port_str += ' '
-				mask <<= 1
-			return obn_cd_port_str
-
 		spacer = '      '
-		mask = 0x00000001
-		obn_cd_str = 'Port1:'
-		obn_cd_str += obn_cd_port(self, flags & 0x0000FFFF)
-		obn_cd_str += '\n' + self._setting_labels[0] + 'Port2:'
-		obn_cd_str += obn_cd_port(self, (flags & 0xFFFF0000) >> 16) + spacer
-		obn_cd_str += '\n'
-		return obn_cd_str
+		obn_cd_port_str = ''
+		for port in range(2):
+			obn_cd_port_str += f'Port{port + 1}:'
+			for line in range(2):
+				if line == 1:
+					obn_cd_port_str += '\n' + self._setting_labels[0] + spacer
+				for i in range(8):
+					obn_cd_port_str += f'{line * 8 + i + 1:2d}:'
+					if flags[port * 16 + line * 8 + i] == 0:
+						obn_cd_port_str += f'{self._off_on_labels[0]:3s}'
+					else:
+						obn_cd_port_str += f'{self._off_on_labels[1]:3s}'
+					obn_cd_port_str += ' '
+			obn_cd_port_str += '\n'
+			if port == 0:
+				obn_cd_port_str += self._setting_labels[0]
+		return obn_cd_port_str
 
 	def __str__(self):
 
@@ -173,48 +169,42 @@ class MIDISettings:
 			f'{self._setting_labels[12]}{self.on_bass_note_chord_detect(self._on_bass_note)}' +				\
 			f'{self._setting_labels[13]}{self.on_bass_note_chord_detect(self._chord_detect)}'
 
-def print_genos_midi_settings(genos_file, analyze=False):
+def print_genos_midi_settings(file_path):
 
-	global console_out
-	console_out = sys.stdout
+	LINE_LABEL_WIDTH = 27
 
-	genos_file_path, genos_file_name = os.path.split(genos_file)
+	genos_file_path, genos_file_name = os.path.split(file_path)
 	genos_file_root, genos_file_ext = os.path.splitext(genos_file_name)
 
 	print(app_hdr_str)
-	print(f'Genos2 File: {genos_file_name}', file=console_out)
+	print(f'{"Genos2 MIDI settings file:":{LINE_LABEL_WIDTH}s} {genos_file_name}')
 
 	# open Genos file
 	if genos_file_ext != '.mis':
-		return f'incorrect file type: "{genos_file_ext}"'
+		print(f'{"incorrect file type:":{LINE_LABEL_WIDTH}s} {genos_file_ext}')
 	try:
-		genos_file_stream = open(genos_file, 'rb')
+		genos_file_stream = open(file_path, 'rb')
 	except FileNotFoundError:
-		return f'could not open file: "{genos_file}"'
+		print(f'{"could not open file:":{LINE_LABEL_WIDTH}s} {file_path}')
 
-	# read file header
+	# read and ignore file header
 	genos_file_stream.read(HDR_LGTH)
 
+	# read MIDI settings from file
 	settings_bytes = genos_file_stream.read(MIDISettings.SETTINGS_LGTH)
 
 	# unpack settings bytes
 	settings = MIDISettings(genos_file_name, settings_bytes)
 
 	# print settings info
-	settings_txt_file = open(os.path.join(genos_file_name + '.txt'), 'w')
-	sys.stdout = settings_txt_file
-	print(settings)
+	txt_file_name = os.path.join(genos_file_root + '.txt')
+	settings_txt_file = open(os.path.join(genos_file_path, txt_file_name), 'w')
+	print(settings, file=settings_txt_file)
 	settings_txt_file.close()
-	sys.stdout = console_out
 
 	genos_file_stream.close()
-
-	return ''
+	return f'{"created text printout:":{LINE_LABEL_WIDTH}s} {txt_file_name}'
 
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		ret_str = print_genos_midi_settings(sys.argv[1], True)
-	else:
-		ret_str = print_genos_midi_settings(sys.argv[1])
-	if ret_str != '':
-		print(f'print_genos_midi_settings() -> {ret_str}', file=console_out)
+	ret_str = print_genos_midi_settings(sys.argv[1])
+	print(ret_str)
